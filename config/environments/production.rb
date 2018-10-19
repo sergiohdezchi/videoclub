4569517553a844e48a8dba95bfefac15d26c12ee
sergio <sergiohdez.chi@gmail.com>
2018-10-19 03:36:31 -0700

    agregando cambios

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   # Verifies that versions and hashed value of the package contents in the project's package.json
- # config.webpacker.check_yarn_integrity = false
+  config.webpacker.check_yarn_integrity = false
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.serve_static_assets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
